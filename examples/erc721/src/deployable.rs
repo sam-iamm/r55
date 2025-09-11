7ecbbba788a8b810add540aeb3e3b4dc820d63ba
sam-iamm <111204792+sam-iamm@users.noreply.github.com>
2025-09-10 21:46:50 -0700

    chore: update auto-generated files tracked in git

---
+++ examples/erc721/src/deployable.rs
@@ -0,0 +1,10 @@
+//! Auto-generated based on Cargo.toml dependencies
+//! This file provides Deployable implementations for contract dependencies
+//! TODO (phase-2): rather than using `fn deploy(args: Args)`, figure out the constructor selector from the contract dependency
+
+use alloy_core::primitives::{Address, Bytes};
+use eth_riscv_runtime::{create::Deployable, InitInterface, ReadOnly};
+use core::include_bytes;
+
+
+
