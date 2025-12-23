39b3b06233c6fd38d60752002528cab97638306b
MMichael9 <michael.minhas@hotmail.com>
2025-12-23 11:29:38 -0800

    syscalls/runtime: add Address syscall (0x30) + helper (#25)

--- eth-riscv-syscalls/src/lib.rs
+++ eth-riscv-syscalls/src/lib.rs
@@ -55,6 +55,7 @@ macro_rules! syscalls {
 // as described on https://www.evm.codes.
 //
 // t0: 0x20, opcode for keccak256, a0: offset, a1: size, returns keccak256 hash
+// t0: 0x30, opcode for address, returns an address
 // t0: 0x32, opcode for origin, returns an address
 // t0: 0x33, opcode for caller, returns an address
 // t0: 0x34, opcode for callvalue, a0: first limb, a1: second limb, a2: third limb, a3: fourth limb, returns 256-bit value
@@ -67,7 +68,7 @@ macro_rules! syscalls {
 // t0: 0x5A, opcode for gas (gasleft), returns 64-bit remaining gas in a0
 // t0: 0xf0, opcode for create, args: a0: 64-bit value, a1: calldata offset, a2: calldata size, returns an address
 // t0: 0xf1, opcode for call, args: a0-a2: address, a3: 64-bit value, a4: calldata offset, a5: calldata size
-// t0: 0xf4, opcode for delegatecall, args: a0-a2: address, a3: MUST be 0, a4: calldata offset, a5: calldata size
+// t0: 0xf4, opcode for delegatecall, args: a0-a2: address, a3: unused, a4: calldata offset, a5: calldata size
 // t0: 0xfa, opcode for staticcall, args: a0-a2: address, a3: 64-bit value, a4: calldata offset, a5: calldata size
 // t0: 0xf3, opcode for return, a0: memory address of data, a1: length of data in bytes, doesn't return
 // t0: 0xfd, opcode for revert, doesn't return
@@ -80,6 +81,7 @@ macro_rules! syscalls {
 syscalls!(
     // EVM opcodes
     (0x20, Keccak256, "keccak256"),
+    (0x30, Address, "address"),
     (0x32, Origin, "origin"),
     (0x33, Caller, "caller"),
     (0x34, CallValue, "callvalue"),
